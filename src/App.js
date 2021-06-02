ae4064e86868edde2b23ceb2869d96cd0f27ce4b
valeriaradchenko <leraradchenko818@gmail.com>
2021-06-02 12:19:13 +0300

    Add MainPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch , Link} from "react-router-dom";
 import { Step1 } from "./Step1";
 import { Step2 } from "./Step2";
 import { Google } from "./Google";
@@ -10,16 +10,27 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import { Navbar } from 'react-bootstrap'
 import logo from './logo.png';
-import GoogleLogin from "react-google-login";
 import Nav from "react-bootstrap/Nav";
-import NavLink from "react-bootstrap/NavLink";
-import GoogleButton from "react-google-button";
-import {Form} from "./components/Form";
 import Button from "@material-ui/core/Button";
-
+import AsyncStorage from "@react-native-community/async-storage";
 
 
 function App() {
+    const [token, setToken] = React.useState(null);
+
+    const getData = async () => {
+        try {
+            const value = await AsyncStorage.getItem('jwt');
+            if(value !== null) {
+                setToken(value);
+                return value;
+            }
+            return null;
+        } catch(e) {
+            console.log(e);
+        }
+    };
+    const userToken = getData();
   return (
     <>
       <Router>
@@ -32,18 +43,18 @@ function App() {
                   <Navbar.Toggle/>
                   <Navbar.Collapse class="nav">
                       <Nav>
-                          <NavLink to="#Main" onClick={() => {
-                              window.location.assign('/');
-                          }}> Main </NavLink>
-                          <NavLink to="#About Us" onClick={() => {
-                              window.location.assign('/About');
-                          }}> About Us </NavLink>
-                          <NavLink to="#Payments" onClick={() => {
-                              window.location.assign('/Pricing');
-                          }}> Pricing </NavLink>
-                          <Button  variant="contained" size="small" color="primary" onClick={() => {
-                              window.location.assign('/Step1');
-                          }} >Sign up</Button>
+                          <Link to="/" className="nav-link">
+                              Main
+                          </Link>
+                          <Link to="/About" className="nav-link">
+                              About Us
+                          </Link>
+                          <Link to="/Pricing" className="nav-link">
+                              Pricing
+                          </Link>
+                          <Link to="/Step1" className="button-link">
+                              <Button variant="contained" size="small" color="primary">Sign up</Button>
+                          </Link>
                       </Nav>
                   </Navbar.Collapse>
               </Navbar>
@@ -51,7 +62,7 @@ function App() {
         <Switch>
             <Route exact path="/" component={Main} />
           <Route path="/Step1" component={Step1} />
-          <Route path="/Step2" component={Step2} />
+          <Route path="/Step2" component={Step2} initialParams={{setToken}}/>
           <Route path="/Google" component={Google} />
           <Route path="/About" component={About} />
           <Route path="/Pricing" component={Pricing} />
